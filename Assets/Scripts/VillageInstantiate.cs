b8c3203fd25acdde54b4ae433288bf6a83a0d95e
Gorungas <50162737+Gorungas@users.noreply.github.com>
2021-10-11 18:47:56 -0400

    Village Instantiation!

--- Assets/Scripts/VillageInstantiate.cs
+++ Assets/Scripts/VillageInstantiate.cs
@@ -6,19 +6,24 @@ public class VillageInstantiate : MonoBehaviour
 {
     public int numVillages;
     public GameObject villagePrefab;
-    public GameObject[] villageArray;
+    public List<GameObject> villageArray = new List<GameObject>();
+    public GameObject[] possibleLocation;
 
     void Start()
     {
-        for (int i = 0; i < villageArray.Length; i++)
+        bool[] checklist=new bool[possibleLocation.Length];
+        for (int i = 0; i < possibleLocation.Length; i++)
         {
-            villageArray[i] = Instantiate(villagePrefab, new Vector2(Random.Range(-10, 10), Random.Range(-10, 10)), Quaternion.identity);
-        }
+            if (villageArray.Count < numVillages)
+            {
+                int index = Random.Range(0, possibleLocation.Length);
+                while (checklist[index]) {
+                    index = Random.Range(0, possibleLocation.Length);
+                }
+                checklist[index] = true;    
+                villageArray.Add(Instantiate(villagePrefab, possibleLocation[index].transform.position, Quaternion.identity));
 
-        for (int i = 0; i < villageArray.Length; i++)
-        {
-            
-            
+            }
         }
     }
 }
