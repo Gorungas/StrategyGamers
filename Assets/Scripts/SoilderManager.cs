4ab8064ef40ba8cfa49340a503e5dee71cbaef8d
JefferyAS <jeffery2001@163.com>
2021-10-11 15:05:51 -0400

    Soldier manager update

--- Assets/Scripts/SoilderManager.cs
+++ Assets/Scripts/SoilderManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SoilderManager : MonoBehaviour
 {
+    public Transform[] soldiersPoses;
+    public List<GameObject> soldiers;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,18 @@ public class SoilderManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void AssignSoldier(GameObject soldier) {
+        int index=soldiers.IndexOf(null);
+        if (index != -1) {
+            soldiers.RemoveAt(index);
+            soldiers.Insert(index, soldier);
+        }
+        else
+        {
+            index = soldiers.Count;
+            soldiers.Add(soldier);
+        }
+        soldier.GetComponent<SoilderFollow>().followPos = soldiersPoses[index];
     }
 }
