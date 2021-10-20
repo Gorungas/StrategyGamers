a8515dd0ebbda2b2773e9fd4548a735f30962f70
JefferyAS <jeffery2001@163.com>
2021-10-20 17:05:55 -0400

    collider

---
+++ Assets/Scripts/DashScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashScript : MonoBehaviour
+{
+    private int playerNum;
+    private string button;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        button = "Dash" + playerNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(button))
+        {
+            
+        }
+    }
+
+}
