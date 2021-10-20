6e750b5f386ef6adbe6dcf2d4a0f85a0e1098a59
JefferyAS <jeffery2001@163.com>
2021-10-20 18:07:34 -0400

    Update ArrowScript.cs

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -43,12 +43,7 @@ public class ArrowScript : MonoBehaviour
         }
         if (collision.CompareTag("Village"))
         {
-            if (collision.GetComponent<PlayerNumberManager>().playerNum != playerNum && !collision.GetComponent<HealthManager>().isDead)
-            {
-                collision.GetComponent<HealthManager>().TakeDamage(damage);
-                Source.PlayOneShot(Hitsound);
-                Destroy(gameObject);
-            }
+            Destroy(gameObject);
         }
     }
 }
