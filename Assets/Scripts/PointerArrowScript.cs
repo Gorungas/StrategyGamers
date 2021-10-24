0d1d62e3cf01518298403f259899617ddfbe34c4
Gorungas <jackson9w9@gmail.com>
2021-10-24 17:29:01 -0400

    indicatorchange

--- Assets/Scripts/PointerArrowScript.cs
+++ Assets/Scripts/PointerArrowScript.cs
@@ -6,13 +6,32 @@ public class PointerArrowScript : MonoBehaviour
 {
     public Transform target;
     public Transform king;
+    public float hideDistance;
     private void Update()
     {
 
         var dir = target.position - king.position;
 
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (dir.magnitude < hideDistance)
+        {
+            SetChildrenActive(false);
+        }
+        else
+        {
+            SetChildrenActive(true);
+
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        
     }
 
+    void SetChildrenActive (bool val)
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(val);
+        }
+    }
 }
