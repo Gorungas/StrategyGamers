9a421a8aff42607e5cceb1d7e1b222ed82e3e636
JefferyAS <jeffery2001@163.com>
2021-10-24 19:07:21 -0400

    char selection

--- Assets/Scripts/PlayerNumberManager.cs
+++ Assets/Scripts/PlayerNumberManager.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class PlayerNumberManager : MonoBehaviour
 {
     public int playerNum;
+    public int charIndex;
     // Start is called before the first frame update
+    void Awake()
+    {
+        if (PublicVars.characters[charIndex]!=-1)
+        {
+            playerNum = PublicVars.characters[charIndex];
+        }
+    }
     void Start()
     {
         
--- Assets/Scripts/SelectMove.cs
+++ Assets/Scripts/SelectMove.cs
@@ -9,17 +9,18 @@ public class SelectMove : MonoBehaviour
     public int playerNum;
     public Transform[] pos;
     private int index = 0;
-    private bool canMove = true;
+    private bool canMove = true,selected=false;
 
     void Start()
     {
         index = playerNum - 1;
         transform.position = pos[index].position;
+        PublicVars.characters = new int[4] { -1, -1, -1, -1 };
     }
 
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal" + playerNum) > 0 && canMove)
+        if (Input.GetAxisRaw("Horizontal" + playerNum) > 0 && canMove&&!selected)
         {
             do
             {
@@ -31,7 +32,7 @@ public class SelectMove : MonoBehaviour
             transform.position = pos[index].position;
             canMove = false;
         }
-        else if (Input.GetAxisRaw("Horizontal" + playerNum) < 0 && canMove)
+        else if (Input.GetAxisRaw("Horizontal" + playerNum) < 0 && canMove && !selected)
         {
             do
             {
@@ -48,23 +49,24 @@ public class SelectMove : MonoBehaviour
             canMove = true;
         }
 
-        if (Input.GetButtonDown("Jump" + playerNum))
+        if (Input.GetButtonDown("Jump" + playerNum)|| Input.GetButtonDown("Attack" + playerNum))
         {
             if (PublicVars.characters[index] == playerNum)
             {
                 PublicVars.characters[index] = -1;
                 pos[index].gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                selected = false;
             }
             else
             {
                 PublicVars.characters[index] = playerNum;
                 pos[index].gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
-                canMove = false;
+                selected = true;
 
                 print("player" + PublicVars.characters[index] + " is " + index);
                 if (!PublicVars.characters.Contains(-1))
                 {
-                    SceneManager.LoadScene("Level1");
+                    SceneManager.LoadScene(2);
                 }
 
             }
